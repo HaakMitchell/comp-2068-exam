24ce87998ac2f30abea6b32dcd91fdcf7881a3e1
Mitchell Haak <200329261@student.georgianc.on.ca>
2020-07-31 19:22:31 -0400

    examdone

---
+++ client/src/components/Routes.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Home from './pages/Home';
+
+import Register from './users/Register';
+import Login from './sessions/Login';
+import Logout from './sessions/Logout';
+
+import Tours from './tours/Index';
+import NewTour from './tours/New';
+import EditTour from './tours/Edit';
+import { Fragment } from "react";
+
+function Routes ({user, setUser}) {
+  return (
+    <Switch>
+      <Route exact path="/" render={
+        renderProps => <Home
+          {...renderProps}
+          user={user}
+        />
+      }/>
+      <Route exact path="/register" render={
+        renderProps => <Register
+          {...renderProps}
+          setUser={setUser}
+        />
+      }/>
+      <Route exact path="/login" render={
+        renderProps => <Login
+          {...renderProps}
+          setUser={setUser}
+        />
+      }/>
+      <Route exact path="/logout" render={
+        renderProps => <Logout
+          {...renderProps}
+          setUser={setUser}
+        />
+      }/>
+      
+      {
+        /*
+          Tricked you!
+          The routes have been completed to provide a better user experience.
+          HOWEVER!!!
+
+          In your own words, please explain what is happening in the logic below.
+          ANSWER HERE: so when you type in the address bar /tours it renders the prop if the user is logged on, and if the user isnt logged on it redirects to / which is the home page.
+
+        */
+      }
+      <Route exact path="/tours" render={
+        props => user ? (
+          <Tours {...props} user={user} />
+        ) : (
+          <Redirect to="/"/>
+        )
+      }/>
+      <Route exact path="/tours/new" render={
+        props => user ? (
+          <NewTour {...props} />
+        ) : (
+          <Redirect to="/"/>
+        )
+      }/>
+      <Route exact path="/tours/edit" render={
+        props => user ? (
+          <EditTour {...props} />
+        ) : (
+          <Redirect to="/"/>
+        )
+      }/>
+    </Switch>
+  );
+}
+
+export default Routes;
